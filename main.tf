36a59e40ae13f89ad7db50829363e5851b6e98f2
hmulya <hmulya@fintax.id>
2022-04-19 02:43:50 +0700

    configure for single instance

--- main.tf
+++ main.tf
@@ -20,9 +20,9 @@ resource "google_compute_network" "vpc_network" {
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
+  name         = "nodeabc"
   machine_type = "f1-micro"
-  tags         = ["web", "dev"]
+  tags         = ["ssh", "alphaclient"]
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-2004-lts"
@@ -33,5 +33,27 @@ resource "google_compute_instance" "vm_instance" {
     access_config {
     }
   }
-  metadata_startup_script = file(var.startup_file)
+  # metadata_startup_script = file(var.startup_file)
+  metadata_startup_script = data.template_file.startup_script.rendered
+}
+
+resource "google_compute_firewall" "ssh-http-rule" {
+  name = "terraform-fw"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports = ["22", "80"]
+  }
+  target_tags = ["alphaclient", "alphaserver"]
+  source_ranges = ["0.0.0.0/0"]
+}
+output "ip_abc" {
+  value = google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip
+}
+
+data "template_file" "startup_script" {
+  template = file("startupscript.sh")
+  vars = {
+    echo_ip = "test123"
+  }
 }
--- startupscript.sh
+++ startupscript.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 scriptpath='/etc/profile.d/startup.sh'
+export ECHO_IP=${echo_ip}
+
 sudo cat << EOF > $scriptpath
 #!/bin/bash
-echo $(date) >> ~/sshres.txt
+echo "\$(hostname) - \$(date) - $ECHO_IP" >> ~/sshres.txt
 EOF
+
 chmod u+x $scriptpath
 source $scriptpath
+cat $scriptpath
