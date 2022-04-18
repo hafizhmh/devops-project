beeb2124c34d2c538cfd67c0a79bfdd29c7c07d7
hmulya <hmulya@fintax.id>
2022-04-19 03:44:53 +0700

    Added post request placeholder

--- startupscript.sh
+++ startupscript.sh
@@ -5,8 +5,11 @@ export ECHO_IP=${echo_ip}
 sudo cat << EOF > $scriptpath
 #!/bin/bash
 echo "\$(hostname) - \$(date) - $ECHO_IP" >> ~/sshres.txt
+curl -X POST https://postman-echo.com/post \
+    -H 'Content-Type: application/json' \
+    -d '{"hostname":"$(hostname)"}' >> ~/curlres.txt
+echo "\n" >> ~/curlsres.txt
 EOF
 
 chmod u+x $scriptpath
 source $scriptpath
-cat $scriptpath
