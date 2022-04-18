dd3d1eb3c370f3f9cc7ebc7270473d723587a7c2
hmulya <hmulya@fintax.id>
2022-04-18 20:29:12 +0700

    Added TF variable placeholder file

--- main.tf
+++ main.tf
@@ -8,13 +8,30 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("google-services.json")
+  credentials = file(var.credentials_file)
 
-  project = "fintax-devops-trial"
-  region  = "us-west1"
-  zone    = "us-west1-a"
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
 
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+  metadata_startup_script = file(var.startup_file)
 }
