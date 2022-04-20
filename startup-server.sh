56ec301c4628cdbf45e054914b0e846af5fe5b98
hmulya <hmulya@fintax.id>
2022-04-20 13:14:36 +0700

    fixed startup script for alpha server

--- startup-server.sh
+++ startup-server.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 export SQL_IP=${sql_ip}
-echo "ok" >> ~/ok.txt
-echo $SQL_IP  ~/sqp_ip.txt
-echo "ok IP" >> ~/ok.txt
-wget https://raw.githubusercontent.com/hafizhmh/devops-project/main/server.py ~
-wget https://raw.githubusercontent.com/hafizhmh/devops-project/main/requirement.txt  ~
-echo "ok wget" >> ~/ok.txt
+echo "ok" >> ~/log.txt
+echo $SQL_IP > sql_ip.txt
+echo $SQL_IP >> ~/log.txt
+echo "ok IP" >> ~/log.txt
+
 apt update
+apt-get install wget -y
 apt install python3 python3-pip -y
+echo "ok install" >> ~/log.txt
+wget 'https://raw.githubusercontent.com/hafizhmh/devops-project/main/server.py' -ct 0 -O ~/server.py
+wget 'https://raw.githubusercontent.com/hafizhmh/devops-project/main/requirement.txt'-ct 0 -O ~/requirement.txt
+echo "ok wget" >> ~/log.txt
+ls -al ~ >> ~/log.txt
 pip3 install -r requirement.txt
-# echo "ok install" >> ~/ok.txt
-# chmod u+x server.py
-# ./server.py &
-# echo "server done" >> ~/ok.txt
+echo "ok pip" >> ~/log.txt
+chmod u+x server.py
+cd ~
+./server.py &
+echo "server done" >> ~/log.txt
